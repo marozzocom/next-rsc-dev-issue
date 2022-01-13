a4b9fed2ddc129ba9fbe6c5ac7db2ad3ca7e1c8b
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2022-01-13 10:07:36 +0200

    initial commit

---
+++ pages/test.server.js
@@ -0,0 +1,11 @@
+export default function Test() {
+  return (
+    <div style={{
+      padding: "1em",
+      margin: "1em",
+      background: "pink"
+    }}>
+      Server rendered
+    </div>
+  )
+}
